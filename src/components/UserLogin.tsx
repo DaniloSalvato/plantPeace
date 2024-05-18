e44edbd17645eba49c9330b35dbf998ee6a14cd2
Bruno Messias <henriqm3ss@gmail.com>
2024-05-17 23:02:31 -0300

    feat:Adicionando-Sales

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -1,3 +1,21 @@
+import React from 'react';
+
 export default function UserLogin() {
-  return <span className="md:mr-16">icon</span>
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        {/* Campo de usuário */}
+        <input
+          type="text"
+          placeholder="Nome de usuário"
+          className="w-full p-2 border rounded"
+        />
+        {/* Botão de login */}
+        <button className="w-full mt-4 bg-blue-500 text-white p-2 rounded">
+          Entrar
+        </button>
+      </div>
+    </div>
+  );
 }
+
